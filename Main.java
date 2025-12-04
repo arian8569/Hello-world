692a92e03e8fe73600e1dafec127823fdbd4c393
Arian <arianghaffari85@icloud.com>
2025-12-05 01:03:23 +0330

    just hello

--- Main.java
+++ Main.java
@@ -1,31 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter Your Name: ");
-        String name = input.nextLine();
-
-        System.out.print("Enter Your age: ");
-        String age = input.nextLine();
-
-        System.out.print("Enter Your nationality: ");
-        String nl = input.nextLine();
-
-        System.out.print("Enter Your phone number: ");
-        String pn = input.nextLine();
-
-        System.out.print("Enter Your email: ");
-        String email = input.nextLine();
-
-        System.out.print("Enter Your address: ");
-        String address = input.nextLine();
-
-        System.out.print("your ID is " + name + age + nl + pn + email + address);
-        System.out.println(":)");
-
+        System.out.println("Hello World");
+        System.out.println("I'm learning Java");
 
     }
 }
